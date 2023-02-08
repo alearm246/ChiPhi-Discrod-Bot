1643d64ebb2d4887af613af471d8494b720e8c6f
alearm246 <47065061+alearm246@users.noreply.github.com>
2023-02-08 14:43:57 -0600

    setup command and event handlers

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
+const fs = require('node:fs');
+const path = require('node:path');
 require("dotenv").config();
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, Collection } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
-const PREFIX = ".";
-
+// Create a new client instance
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds, 
     GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.MessageContent] });
+    GatewayIntentBits.MessageContent] 
+});
 
 client.commands = new Collection();
 
-client.on("ready", () => {
-    console.log("Bot is online!");
-    client.user.setActivity("Watching Server", { type: "WATCHING" });
-});
+//commands
+const commandsPath = path.join(__dirname + "/commands");
+const commandsFile = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
-client.on("messageCreate", (message) => {
-    if (message.author.bot) return;
-    const validGreetings = ["hello", "hi", "yo", "wassup"];
-    if (validGreetings.includes(message.content.toLowerCase().replace(/\s/g,''))) {
-        message.reply("hello");
-    }
-})
+for (const file of commandsFile) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
+//events
+const eventsPath = path.join(__dirname, '/events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(process.env.TOKEN);
