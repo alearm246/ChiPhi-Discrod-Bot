6eccdf5bd83a4bd6265caa951a633dd6232548f0
alearm246 <47065061+alearm246@users.noreply.github.com>
2023-02-08 15:47:20 -0600

    implement rock paper scissors game

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js");
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with Pong!"),
     async execute(interaction) {
-        await interaction.reply("Pong!");
+        await interaction.deferReply();
+        await wait(5000);
+        //await interaction.reply({ content: "Ping!", ephemeral: true });
+        await interaction.followUp("Ping again without it being secret!");
+        await wait(2000);
+        await interaction.editReply("lolololol");
     }
 }
---
+++ src/commands/rockPaperScissors.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("rock-paper-scissors")
+		.setDescription("let's the user play a round of rock paper scissors")
+		.addStringOption(option =>
+			option.setName("rock-paper-scissors")
+				.setDescription("choose either rock, paper, or scissors")
+				.setRequired(true)
+				.addChoices(
+					{ name: "rock", value: "rock"},
+                    { name: "paper", value: "paper"},
+                    { name: "scissors", value: "scissors"}
+				)),
+    async execute(interaction) {
+        const computerChoices = ["rock", "paper", "scissor"];
+        const computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length - 1)];
+        const userChoice = interaction.options.getString("rock-paper-scissors");
+        if (computerChoice === "rock" && userChoice === "paper") {
+            await interaction.reply("Nice job! You won!");
+        } else if (computerChoice === "rock" && userChoice === "scissors") {
+            await interaction.reply("Dam...can't believe you lost like that");
+        } else if (computerChoice === "paper" && userChoice === "scissors") {
+            await interaction.reply("Nice job! You won!");
+        } else if (computerChoice === "paper" && userChoice === "rock") {
+            await interaction.reply("Dam...can't believe you lost like that");
+        } else if (computerChoice === "scissors" && userChoice === "rock") {
+            await interaction.reply("Nice job! You won!");
+        } else if (computerChoice === "scissors" && userChoice === "paper") {
+            await interaction.reply("Dam...can't believe you lost like that");
+        } else {
+            await interaction.reply("Dang we tied!");
+        }
+        await interaction.followUp(`I threw ${computerChoice} and you threw ${userChoice}`);
+    }
+}
