bbc0bdf049ca4270e93bcdf4f63a4b8ea2dc0514
alearm246 <47065061+alearm246@users.noreply.github.com>
2023-02-09 17:57:23 -0600

    getCreed ccommand

--- src/plugins/Houses/index.js
+++ src/plugins/Houses/index.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "Houses",
     commands: [
         require("./commands/get-quotes"),
+        require("./commands/get-creed.js")
     ],
     components: {
         buttons: [
