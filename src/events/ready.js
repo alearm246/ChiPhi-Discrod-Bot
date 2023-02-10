c224b83bf3dbd9a9d710a7b61ac07c053569478a
alearm246 <alearm246@gmail.com>
2023-02-09 21:28:18 -0600

    add env var to loadup bot

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,10 @@
 const { Events } = require("discord.js");
+require("dotenv").config();
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}!!!`);
+		console.log(`Ready! Logged in as ${client.user.tag} in the ${process.env.environment}`);
 	},
 };
